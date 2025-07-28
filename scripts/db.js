6f0822d7aae9fe9244fe88365254f0fec0c54aba
Benjamin Talinski <benny13t@gmx.de>
2025-07-28 14:35:16 +0200

    first commit

---
+++ scripts/db.js
@@ -0,0 +1,54 @@
+let myDishes = [
+    {
+        "name": "Gericht 1",
+        "price": 1.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 2",
+        "price": 2.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 3",
+        "price": 3.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 4",
+        "price": 4.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 5",
+        "price": 5.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 6",
+        "price": 6.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 7",
+        "price": 7.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 8",
+        "price": 8.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 9",
+        "price": 9.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    {
+        "name": "Gericht 10",
+        "price": 10.00,
+        "description": "Hier ist die Beschreibung!"
+    },
+    
+    
+]
